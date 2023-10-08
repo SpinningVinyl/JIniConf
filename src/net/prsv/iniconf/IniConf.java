d2c66711918a37b737781f304f15179c3f9a4555
Pavel Urusov <pavel.urusov@gmail.com>
2023-10-08 19:17:54 +0100

    rename IniConfDict to IniConf

--- src/net/prsv/iniconf/IniConf.java
+++ src/net/prsv/iniconf/IniConf.java
@@ -1,12 +1,15 @@
 package net.prsv.iniconf;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
-public class IniConfDict {
+public class IniConf {
     private final HashMap<String, String> properties;
-    private final HashMap<String, IniConfDict> subsections;
+    private final HashMap<String, IniConf> subsections;
 
-    public IniConfDict() {
+    public IniConf() {
         properties = new HashMap<>();
         subsections = new HashMap<>();
     }
@@ -32,13 +35,42 @@ public class IniConfDict {
     }
 
     /**
+     * Returns the value associated with the specified key, or {@code defaultValue} if this map contains no mapping
+     * for the key.
+     * @param key the key whose associated values is to be returned
+     * @param defaultValue the default value to be returned if there is no value associated with the specified key
+     * @return the value to which the specified key is mapped, or {@code defaultValue} if this map contains no mapping for the key
+     */
+    public String getOrDefault(String key, String defaultValue) {
+        return properties.getOrDefault(key, defaultValue);
+    }
+
+    /**
+     * Checks whether the dict contains the specified key.
+     * @param key the key to be checked
+     * @return {@code true} if the dict contains the specified key, {@code false} otherwise.
+     */
+    public boolean isKey(String key) {
+        return properties.containsKey(key);
+    }
+
+    /**
+     * Checks whether this dict contains a subsection with the specified name.
+     * @param sectionName section name to be checked
+     * @return {@code true} if this dict has a subsection with the specified name, {@code false} otherwise.
+     */
+    public boolean isSubsection(String sectionName) {
+        return subsections.containsKey(sectionName);
+    }
+
+    /**
      * Returns the subsection associated with the specified subsection name, or {@code null}
      * if no such subsection exists.
      * @param name the name of subsection to be returned
      * @return subsection associated with the specified subsection name, or {@code null}
      *         if no such subsection exists
      */
-    public IniConfDict getSubsection(String name) {
+    public IniConf getSubsection(String name) {
         return subsections.get(name);
     }
 
@@ -50,16 +82,16 @@ public class IniConfDict {
      * @return the subsection previously associated with the specified name, or {@code null} if there was no such
      * subsection.
      */
-    public IniConfDict addSubsection(String name, IniConfDict subsection) {
+    public IniConf addSubsection(String name, IniConf subsection) {
         return subsections.put(name, subsection);
     }
 
-    protected HashMap<String, String> getProperties() {
-        return properties;
+    public Map<String, String> getProperties() {
+        return Collections.unmodifiableMap(properties);
     }
 
-    protected HashMap<String, IniConfDict> getSubsections() {
-        return subsections;
+    public Map<String, IniConf> getSubsections() {
+        return Collections.unmodifiableMap(subsections);
     }
 
 }
