ee18de401947e2125a69fc102fa6f0c04d6eff74
Pavel Urusov <pavel.urusov@gmail.com>
2023-10-08 12:28:36 +0100

    IniConfDict: improvements to the basic API

--- src/net/prsv/iniconf/IniConfDict.java
+++ src/net/prsv/iniconf/IniConfDict.java
@@ -16,11 +16,10 @@ public class IniConfDict {
      * for the key, the old value is replaced.
      * @param key key with which the specified value is to be associated
      * @param value value to be associated with the specified key
-     * @return this IniConfDict object
+     * @return the value previously associated with the specified key, or {@code null} if there was no such value
      */
-    public IniConfDict put(String key, String value) {
-        properties.put(key, value);
-        return this;
+    public String put(String key, String value) {
+        return properties.put(key, value);
     }
 
     /**
@@ -32,13 +31,27 @@ public class IniConfDict {
         return properties.get(key);
     }
 
+    /**
+     * Returns the subsection associated with the specified subsection name, or {@code null}
+     * if no such subsection exists.
+     * @param name the name of subsection to be returned
+     * @return subsection associated with the specified subsection name, or {@code null}
+     *         if no such subsection exists
+     */
     public IniConfDict getSubsection(String name) {
         return subsections.get(name);
     }
 
+    /**
+     * Associates the specified subsection with the specified subsection name. If the dict previously had a subsection
+     * with the same name, the old subsection is replaced with the new one.
+     * @param name the name of subsection to be added
+     * @param subsection the subsection to be associated with the specified name
+     * @return the subsection previously associated with the specified name, or {@code null} if there was no such
+     * subsection.
+     */
     public IniConfDict addSubsection(String name, IniConfDict subsection) {
-        subsections.put(name, subsection);
-        return this;
+        return subsections.put(name, subsection);
     }
 
 }
